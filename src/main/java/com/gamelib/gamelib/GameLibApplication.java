3def6e786ad09ded726dcea61756ef2d9558f2c9
delirium <antimonov.m@yandex.ru>
2025-03-10 06:50:23 +0300

    init commit

---
+++ src/main/java/com/gamelib/gamelib/GameLibApplication.java
@@ -0,0 +1,11 @@
+package com.gamelib.gamelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GameLibApplication.class, args);
+    }
+}
