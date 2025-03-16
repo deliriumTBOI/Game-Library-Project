7f9a86360da75c99599b2a0e1c07442f4ebf59da
delirium <antimonov.m@yandex.ru>
2025-03-16 20:26:36 +0300

    init commit

--- src/main/java/com/gamelib/gamelib/repository/GameRepository.java
+++ src/main/java/com/gamelib/gamelib/repository/GameRepository.java
@@ -4,7 +4,6 @@ import com.gamelib.gamelib.model.Company;
 import com.gamelib.gamelib.model.Game;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -33,6 +32,6 @@ public class GameRepository {
     public List<Game> findByCompanyName(String companyName) {
         return games.stream()
                 .filter(g -> g.getCompany().getName().equalsIgnoreCase(companyName))
-                .collect(Collectors.toList());
+                .toList();
     }
 }
